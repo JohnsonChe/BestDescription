eec2459621caba9dbe193a5a67f1851ddd513329
JohnsonChe <Johnson.Che01@gmail.com>
2015-11-11 14:59:18 -0500

    Created QuestionAnswerFragment XML and java files. Attached QuestionAnswer Fragment onto game_activity.xml

---
+++ app/src/main/java/johnson/bestdescription/QuestionAnswerFragment.java
@@ -0,0 +1,17 @@
+package johnson.bestdescription;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class QuestionAnswerFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.questionanswers_fragment,container,false);
+        return view;
+    }
+}
