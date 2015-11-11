6a6594cf8164d44e3af56774ce101d2d66cad7a0
JohnsonChe <Johnson.Che01@gmail.com>
2015-11-11 02:30:09 -0500

    Created two fragments for both question and answers.

--- app/src/main/java/johnson/bestdescription/gameActivity.java
+++ app/src/main/java/johnson/bestdescription/gameActivity.java
@@ -1,11 +1,38 @@
 package johnson.bestdescription;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-/**
- * Created by Owner on 10/17/2015.
- */
-public class gameActivity extends Activity {
+public class GameActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.game_activity);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
