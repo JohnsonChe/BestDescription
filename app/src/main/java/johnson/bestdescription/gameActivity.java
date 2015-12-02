39e40b3a6b3e10b0e55008706944ebf3e9aab6a6
JohnsonChe <Johnson.Che01@gmail.com>
2015-12-02 00:15:00 -0500

    Randomized the possibly choices instead of the choices being at a fixed button location.
    Used ArrayList to store questions from the CurrentQuestion object.
    Implemented:
           moveToString(currentQuestion)
              populates String List
    
     Randomly selects a number from 0 to 3
     That number is the retrieval index from the ArrayList.
     Display the text onto button.
     Deletes the index from the ArrayList.
     Repeat until all buttons contain text.

--- app/src/main/java/johnson/bestdescription/gameActivity.java
+++ app/src/main/java/johnson/bestdescription/gameActivity.java
@@ -1,9 +1,13 @@
 package johnson.bestdescription;
 
+import java.util.Random;
+import java.util.ArrayList;
+
 import android.content.Intent;
 import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,16 +20,27 @@ import java.util.List;
 
 public class gameActivity extends AppCompatActivity {
 
+    public final String TAG = "johnson.bestdescription";
     public int currentQuestionNumber = 0;
+    List<QuestionAnswer> questions;
+    ArrayList<String> CurrentQuestionList = new ArrayList<>(4);
+    QuestionAnswer currentQuestion;
+    int randNum;
+    int maxNum = 4;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.game_activity);
-        List<QuestionAnswer> questions = setQuestionsFromDB();
-        QuestionAnswer currentQuestion = setCurrentQuestion(questions);
+        questions = setQuestionsFromDB();
+        currentQuestion = setCurrentQuestion(questions);
         setQuestionWidgets(currentQuestion);
+
+        currentQuestionNumber = 0;
+
     }
 
     public List<QuestionAnswer> setQuestionsFromDB(){
@@ -50,13 +65,19 @@ public class gameActivity extends AppCompatActivity {
     }
 
     public void onButtonClick(View V){
+        currentQuestionNumber++;
+        currentQuestion = questions.get(currentQuestionNumber);
+        //setCurrentQuestion(questions);
+        setQuestionWidgets(currentQuestion);
+
         //Intent i = new Intent(this,gameActivity.class);
         //startActivity(i);
     }
 
     public QuestionAnswer setCurrentQuestion(List<QuestionAnswer> questions){
         QuestionAnswer currentQuestion = questions.get(currentQuestionNumber);
-        currentQuestionNumber++;
+
+        //currentQuestionNumber++;
 
         return currentQuestion;
     }
@@ -66,16 +87,63 @@ public class gameActivity extends AppCompatActivity {
         questionText.setText(currentQuestion.getQuestion());
 
         Button buttonA = (Button)findViewById(R.id.buttonA);
-        buttonA.setText(currentQuestion.getAnswer());
-
         Button buttonB = (Button)findViewById(R.id.buttonB);
-        buttonB.setText(currentQuestion.getIncorrect1());
-
         Button buttonC = (Button)findViewById(R.id.buttonC);
-        buttonC.setText(currentQuestion.getIncorrect2());
-
         Button buttonD = (Button)findViewById(R.id.buttonD);
-        buttonD.setText(currentQuestion.getIncorrect3());
+        //randomNumber();
+
+
+
+        CurrentQuestionList = moveToString(currentQuestion); //CRASHES HERE
+
+        //randomNumber();
+
+        buttonA.setText(CurrentQuestionList.get(randomNumber()));
+        removeFromQuestionList(CurrentQuestionList);
+        buttonB.setText(CurrentQuestionList.get(randomNumber()));
+        removeFromQuestionList(CurrentQuestionList);
+        buttonC.setText(CurrentQuestionList.get(randomNumber()));
+        removeFromQuestionList(CurrentQuestionList);
+        buttonD.setText(CurrentQuestionList.get(randomNumber()));
+        removeFromQuestionList(CurrentQuestionList);
+
+    }
+
+    public int randomNumber(){
+        Random random = new Random();
+        int thisRandomNumber = random.nextInt(maxNum);
+        Log.d(TAG,"randNum's Value");
+
+
+        maxNum -= 1;
+        if(maxNum == 0)
+            maxNum = 4;
+
+        randNum = thisRandomNumber;
+        return thisRandomNumber;
+    }
+
+    public void removeFromQuestionList (ArrayList<String> CurrentQuestionList){
+        CurrentQuestionList.remove(randNum);
+    }
+
+    public ArrayList<String> moveToString(QuestionAnswer currentQuestion){
+        String q1 = currentQuestion.getAnswer();
+        String q2 = currentQuestion.getIncorrect1();
+        String q3 = currentQuestion.getIncorrect2();
+        String q4 = currentQuestion.getIncorrect3();
+
+        ArrayList<String> stringArrayList = new ArrayList<>(4);
+        populateStringList(stringArrayList, q1, q2, q3, q4);
+
+        return stringArrayList;
+    }
+
+    public void populateStringList(ArrayList<String> stringArrayList,String q1,String q2,String q3,String q4){
+        stringArrayList.add(q1);
+        stringArrayList.add(q2);
+        stringArrayList.add(q3);
+        stringArrayList.add(q4);
     }
 
     @Override
